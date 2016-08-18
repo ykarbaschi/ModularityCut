3be9c8482a2c0c6fbd2a1e7150b254b1eb2c8d79
ykarbaschi <ykarbaschi@yahoo.com>
2016-08-17 19:17:54 -0500

    add main classes for datasets

---
+++ src/modularityCut/MCT.java
@@ -0,0 +1,30 @@
+package modularityCut;
+
+import Jama.Matrix;
+
+import java.util.ArrayList;
+
+public class MCT {
+
+    public static void main(String[] args){
+        SocialGraph data1Cam1 = new SocialGraph();
+        String address = "MCT_Challenge//annotation//Dataset1//Cam1.dat";
+
+        ModularityMeasure modularityMeasure = new ModularityMeasure(
+                data1Cam1.interpretDataMCTChallenge(
+                        data1Cam1.readDataSet(address)));
+
+        ArrayList<Matrix> structure = modularityMeasure.findCommunityStructure();
+
+        Matrix LastGrouping = structure.get(structure.size()-1);
+
+        for (int i = 0; i < LastGrouping.getColumnDimension(); i++) {
+            System.out.println("Group " + i + " Has:");
+            for (int j = 0; j < LastGrouping.getRowDimension(); j++) {
+                if (LastGrouping.get(j, i) == 1 )
+                    System.out.print(j + ", ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/modularityCut/PETSDataSet.java
@@ -0,0 +1,30 @@
+package modularityCut;
+
+import Jama.Matrix;
+
+import java.util.ArrayList;
+
+public class PETSDataSet {
+
+    public static void main(String[] args){
+        SocialGraph PETS_S2L1 = new SocialGraph();
+        String address = "PETS09-S2L1//gt.txt";
+
+        ModularityMeasure modularityMeasure = new ModularityMeasure(
+                PETS_S2L1.interpretDataPETS(
+                        PETS_S2L1.readDataSet(address)));
+
+        ArrayList<Matrix> structure = modularityMeasure.findCommunityStructure();
+
+        Matrix LastGrouping = structure.get(structure.size()-1);
+
+        for (int i = 0; i < LastGrouping.getColumnDimension(); i++) {
+            System.out.println("Group " + i + " Has:");
+            for (int j = 0; j < LastGrouping.getRowDimension(); j++) {
+                if (LastGrouping.get(j, i) == 1 )
+                    System.out.print((j+1) + ", ");
+            }
+            System.out.println();
+        }
+    }
+}
